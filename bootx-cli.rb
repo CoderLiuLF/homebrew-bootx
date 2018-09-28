3112046d166f6f51fd1015fadea7d7958e530a06
猫爸 <maoba@dtstack.com>
2018-09-28 15:14:50 +0800

    update

--- bootx-cli.rb
+++ bootx-cli.rb
@@ -6,10 +6,15 @@ class BootxCli < Formula
   version '1.0.0-SNAPSHOT'
   sha256 '8772097b185d2084eac2d73ffc0f817ec6d0d091c514480b6d2cf90d6104559f'
 
-  def install
-    root = '.'
+  bottle :unneeded
+  depends_on :java
 
-    bin.install Dir["#{root}/bin/bootx-cli"]
-    lib.install Dir["#{root}/lib/bootx-cli.jar"]
+  def install
+  	rm Dir["*.bat"]
+    mv "boot-cli.sh", "boot-cli"
+    chmod 0755, "boot-cli"
+
+    libexec.install Dir["*"]
+    (bin/"boot-cli").write_env_script(libexec/"boot-cli", Language::Java.java_home_env)
   end
 end
