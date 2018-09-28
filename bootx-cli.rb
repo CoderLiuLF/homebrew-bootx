e45596db91c47e892ebe2762283c2e14f4e84017
猫爸 <maoba@dtstack.com>
2018-09-28 15:29:03 +0800

    update

--- bootx-cli.rb
+++ bootx-cli.rb
@@ -4,7 +4,7 @@ class BootxCli < Formula
   homepage 'https://www.dtstack.com/'
   url 'https://dtstack-static.oss-cn-hangzhou.aliyuncs.com/dt-bootx/dt-bootx-cli-1.0.0-SNAPSHOT.tar.gz'
   version '1.0.0-SNAPSHOT'
-  sha256 '8772097b185d2084eac2d73ffc0f817ec6d0d091c514480b6d2cf90d6104559f'
+  sha256 'a4764c91bd8fcd045f5a548aeec780e212a9ef527eb02ed5d504f8f4a5f97bb1'
 
   bottle :unneeded
   depends_on :java
