aae1568901660c251441e15c219685c64754e8b9
猫爸 <maoba@dtstack.com>
2018-09-28 13:57:46 +0800

    update sha 256

--- bootx-cli.rb
+++ bootx-cli.rb
@@ -4,7 +4,7 @@ class BootxCli < Formula
   homepage 'https://www.dtstack.com/'
   url 'https://dtstack-static.oss-cn-hangzhou.aliyuncs.com/dt-bootx/dt-bootx-cli-1.0.0-SNAPSHOT.tar.gz'
   version '1.0.0-SNAPSHOT'
-  sha256 'c01b0bda90478e8f16165ad7d6e78acdc9f657a9'
+  sha256 '8772097b185d2084eac2d73ffc0f817ec6d0d091c514480b6d2cf90d6104559f'
 
   def install
     root = '.'
